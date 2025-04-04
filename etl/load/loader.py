b2408f53a35bb1fa54c532d7b1a0222d387a1993
HoViet0107 <112065031+HoViet0107@users.noreply.github.com>
2025-04-04 14:16:26 +0700

    Add and implement save transformed data as parquet to s3

--- etl/load/loader.py
+++ etl/load/loader.py
@@ -1,10 +1,13 @@
 # Imports
 from etl.spark_initializer import SparkInitializer
+from etl.config.aws_config import AWSConfig
 
 class Loader:
     def __init__(self):
         pass
     def load_to_db(self, df, table_name, db_config, connection_properties):
+        pass
+    def load_to_s3(self, df, s3_folder_name):
         pass
     
 class LoadDataToMysql(Loader):
@@ -28,4 +31,26 @@ class LoadDataToMysql(Loader):
             
         except Exception as e:
             print(f"Error loading data to MySQL: {e}")
+            
+    def load_to_s3(self, df, s3_folder_name):
+        """
+        Save transformed data to S3 as a Parquet file.
+        """
+        try:
+            # Initialize Spark
+            spark = SparkInitializer.get_spark()
+            
+            # Get AWS configuration
+            aws_config = AWSConfig()
+            
+            # Define the S3 path for the Parquet file
+            s3_path = f"s3a://{aws_config.bucket_name}/parquet_data/{s3_folder_name}/"
+            
+            # Write the DataFrame to S3 in Parquet format
+            df.write.mode("overwrite").parquet(s3_path)
+            
+            print(f"Data successfully saved to S3 as Parquet at: {s3_path}")
+        
+        except Exception as e:
+            print(f"Error saving data to S3 as Parquet: {e}")
         
